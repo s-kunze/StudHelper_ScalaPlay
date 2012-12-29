a9ef53b7668b0b53dcd0933bbb4d0a43ea62aeae
Stefan Kunze <stefan@laptop-home.(none)>
2012-12-29 21:33:25 +0100

    Add First example

--- StudHelper/app/controllers/User.scala
+++ StudHelper/app/controllers/User.scala
@@ -1,14 +1,20 @@
 package controllers
 
+import com.codahale.jerkson.Json
+import play.api.mvc.Action
 import play.api.mvc.Controller
 
 import transfer._
 
 object User extends Controller {
-
+  
   def getAll = TODO
   
-  def get(id: Long) = TODO
+  def get(id: Long) = Action { 
+	val user = UserTransfer(id,"Stefan", "Kunze", "test",3)
+    val jsonString = Json.generate(user)
+    Ok(jsonString).as("application/json")
+}
   
   def create(id: Long) = TODO
  
